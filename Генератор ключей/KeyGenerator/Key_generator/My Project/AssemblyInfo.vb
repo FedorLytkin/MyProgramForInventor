c0573392f15b798ea4e6c825fdb12dabbcbf308d
FedorLytkin <39435645+FedorLytkin@users.noreply.github.com>
2018-11-20 11:18:21 +0300

    v1.0.3.9
    *Изменена логика "прохода" по элементам в BOM. В условие добавлена проверка на тип файла по его двоичному значению
    + добавлена свойство ToolTrip. Описаны все элементы интерфейса
    * изменена процедура открытия формы Показать имя файла. Теперь откывается через ShowDialog

---
+++ Генератор ключей/KeyGenerator/Key_generator/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Key_generator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Key_generator")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("5e30ae65-d72e-4723-836e-58c6d91bde92")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
